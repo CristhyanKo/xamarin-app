a363005dea63dec46cb6772bed89cb331436bd25
Cristhyan Kohlhase <cristhyan.ko@outlook.com>
2018-08-07 10:56:00 -0400

    APP 01 - Concluído Adicionais: Async.

---
+++ Seção 05/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using App01_ConsultarCEP.Service;
+using App01_ConsultarCEP.Service.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App01_ConsultarCEP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BOTAO.Clicked += BuscarCep;
+        }
+
+        private async void BuscarCep(object sender, EventArgs args)
+        {
+            try
+            {
+                string cep = CEP.Text.Trim();
+
+                ValidaCep(cep);
+
+                Endereco end = await ViaCepService.BuscarEnderecoViaCep(cep);
+                RESULTADO.Text = $"Cidade: {end.Localidade}   |   Estado: {end.Uf}   |   Logradouro: {end.Logradouro}   |   Bairro: {end.Bairro}";
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Atenção", ex.Message, "OK");
+            }
+        }
+
+        private void ValidaCep(string cep)
+        {
+            int novoCEP = 0;
+            if (string.IsNullOrEmpty(cep)) throw new Exception("Informe um CEP.");
+            if (cep.Length != 8) throw new Exception("O CEP deve conter 8 digitos.");
+            if (!int.TryParse(cep, out novoCEP)) throw new Exception("O CEP deve conter apenas números.");
+        }
+    }
+}
---
+++ Seção 05/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/Service/ViaCepService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App01_ConsultarCEP.Service.Model;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace App01_ConsultarCEP.Service
+{
+    public class ViaCepService
+    {
+        private static string EnderecoURL = "https://viacep.com.br/ws/{0}/json/";
+
+        public static async Task<Endereco> BuscarEnderecoViaCep(string cep)
+        {
+            WebClient wc = new WebClient();
+
+            string novoEnderecoURL = string.Format(EnderecoURL, cep);
+            string content = await wc.DownloadStringTaskAsync(novoEnderecoURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(content);
+
+            if (end.Cep == null) throw new Exception("CEP inválido.");
+
+            return end;
+        }
+    }
+}
